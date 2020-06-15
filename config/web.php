c53fb31d28942f363235cec09c8dafe157153955
Oscar Megía López <pfgytk@gmail.com>
2020-06-15 12:54:47 +0200

    3) Configuración

--- config/web.php
+++ config/web.php
@@ -4,7 +4,9 @@ $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
-    'id' => 'basic',
+    'id' => 'HojaDeEjercicios5DeYii2',
+    'name' =>  'Hoja de Ejercicios 5 de Yii2',
+    'defaultRoute' => 'site/inicio',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
@@ -43,14 +45,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
-            'showScriptName' => false,
+            'showScriptName' => true,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
